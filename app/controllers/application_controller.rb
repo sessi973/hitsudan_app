97ac913bc901b9bc4117f5c74538fb1e780e4b35
sessi973 <hatano.seiko@gmail.com>
2022-03-25 17:51:26 +0900

    ベーシック認証再設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?  
 
-  private
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
